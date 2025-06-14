07c7dd5ebc0407840b58a08cb49e8811d67c0c5a
zuhayr <zuhayr202@gmail.com>
2025-06-14 14:16:30 -0500

    feat: add swagger docs and update homepage

--- src/index.ts
+++ src/index.ts
@@ -44,6 +44,7 @@ app.get('/', (c) => {
       </head>
       <h2>Avatar: The Last Airbender Quotes API</h2>
       <h2><a href="https://github.com/zuhayrali/atla-quotes-api" target="_blank" rel="noopener noreferrer">Source Code (GitHub)</a></h2>
+      <h2><a href="${new URL(c.req.url).origin}/dist/index.html" target="_blank" rel="noopener noreferrer">Swagger</a></h2>
       <p>Developed for plugin development on <a href="https://usetrmnl.com/" target="_blank" rel="noopener noreferrer">trmnl</a>.<p>
       <h3>Current Quotes per Character</h3>
       <ul>
@@ -58,7 +59,7 @@ app.get('/', (c) => {
   HTML += `
   <h3>TODO</h3>
   <ul>
-    <li>[  ] OpenAPI / Swagger docs </li>
+    <li>[ x ] OpenAPI / Swagger docs </li>
     <li>[  ] Add 10 quotes per character </li>
     <li>[ x ] Add 3 1-bit images per character </li>
     <li>[ x ] Add image url to quote response </li>
